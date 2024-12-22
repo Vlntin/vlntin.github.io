8cfda52937572d04df5fa6db8e0bf40cc82110fe
Valentin Marotta <valentin.marotta@web.de>
2024-12-22 18:10:19 +0100

    next

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb40d94055c1be301da6cda2706826d15ea52b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3044099820"
+    serviceWorkerVersion: "1387402949"
   }
 });
